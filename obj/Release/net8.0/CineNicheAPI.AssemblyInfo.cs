f6cbd7848ce4d665abe8ab5702d005dc42d7964a
westonrwatson <westonrwatson@gmail.com>
2025-04-11 00:37:49 -0600

    final edits

--- obj/Release/net8.0/CineNicheAPI.AssemblyInfo.cs
+++ obj/Release/net8.0/CineNicheAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineNicheAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c7dba92729fed0876f80dd2e57e071ed6afb617")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d2482ad3eebecebc05832ec5d6e962bdded526")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineNicheAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineNicheAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
